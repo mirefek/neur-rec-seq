020f68c932a52905ab751c31599f0b173de28cc2
mirefek <mirek@olsak.net>
2020-06-13 16:19:54 +0200

    500 epochs in lstm experiment

--- sort_proc_lstm.py
+++ sort_proc_lstm.py
@@ -158,7 +158,7 @@ if __name__ == "__main__":
         },
         'train' : {
             'batch_size' : 10,
-            'epochs' : 10,
+            'epochs' : 500,
             'optimizer' : {
                 'type' : 'Adam'
             },
